0a1f71c610c0dba126c0dc5cbe5664c779efd332
Shady <shadyxgottwald99@gmail.com>
2023-07-09 18:46:36 +0200

    TaskList Stage 1 code.

--- Tasklist/task/src/tasklist/Menu.kt
+++ Tasklist/task/src/tasklist/Menu.kt
@@ -1,4 +1,24 @@
 package tasklist
+import java.util.Scanner;
+interface MenuFunction {
+    fun getAction(scan: Scanner): Unit
+}
 
-enum class Menu {
+enum class Menu(val mode: String):MenuFunction {
+
+    ADD("add"){
+        override fun getAction(scan: Scanner): Unit = scan.use{ sc->
+                while(sc.hasNextLine()) {
+                    val task: String = scan
+                        .nextLine()
+                        .lineSequence()
+                        .joinToString()
+                        .trim()
+
+                    if(task.isBlank()) return
+                }
+            }
+
+    }
+
 }
--- Tasklist/task/src/tasklist/Task.kt
+++ Tasklist/task/src/tasklist/Task.kt
@@ -1,5 +1,7 @@
 package tasklist
 
 data class Task(val content: String) {
+    val tasks = ArrayList<String>()
+
     override fun toString(): String = content
 }
